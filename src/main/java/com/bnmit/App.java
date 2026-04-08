35c814756fd6e4978f0b569aa4a51039ccb7a31e
Prajwal-Gowda-N <prajwal.g12305@gmail.com>
2026-04-09 01:10:52 +0530

    "D"

--- src/main/java/com/bnmit/App.java
+++ src/main/java/com/bnmit/App.java
@@ -1,23 +1,28 @@
 package com.bnmit;
 
-/**
- * Hello world!
- */
 public class App {
-    int balance =0;
-     public int deposite(int amount) {
-    	 balance += amount;
-    	 return balance;
-     }
-     
-     public int withdraw(int amount) {
-    	 if(amount<balance) {
-    		 balance -= amount;
-    		 return balance;
-    	 }
-    	 return balance;
-     }
-     public int get_balance() {
-    	 return balance;
-     }
+    int balance = 0;
+
+    public int deposite(int amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public int withdraw(int amount) {
+        if (amount < balance) {
+            balance -= amount;
+        }
+        return balance;
+    }
+
+    public int get_balance() {
+        return balance;
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.deposite(1000);
+        app.withdraw(200);
+        System.out.println(app.get_balance());
+    }
 }
