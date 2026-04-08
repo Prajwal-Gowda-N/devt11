02b6f8b23ffe5a182687f0a5983ab7ce1b8877f4
Prajwal-Gowda-N <prajwal.g12305@gmail.com>
2026-04-09 01:36:57 +0530

    Update App.java

--- src/main/java/com/bnmit/App.java
+++ src/main/java/com/bnmit/App.java
@@ -4,10 +4,15 @@ import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
 
 public class App {
+
+    public static String greet() {
+        return "Hello from Greeting App";
+    }
+
     public static void main(String[] args) throws Exception {
         HttpServer s = HttpServer.create(new InetSocketAddress(6001), 0);
         s.createContext("/", e -> {
-            byte[] r = "Hello from Greeting App".getBytes();
+            byte[] r = greet().getBytes();
             e.sendResponseHeaders(200, r.length);
             e.getResponseBody().write(r);
             e.close();
